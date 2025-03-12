60e74836bf5bc3ed19e6e303efc3a28eb158af52
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-03-12 08:51:02 +0100

    updated order

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,13 @@
 import { serve } from "inngest/next";
 
-import { inngest, syncUserCreation, syncUserDelete, syncUserUpdate } from "../../../config/inngest";
+import { createUserOrder, inngest, syncUserCreation, syncUserDelete, syncUserUpdate } from "../../../config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
     syncUserUpdate,
-    syncUserDelete
+    syncUserDelete,
+    createUserOrder
   ],
 });
